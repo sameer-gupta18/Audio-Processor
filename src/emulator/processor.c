3f167e422aa768b907c381f2fccf3a6d3c1fc8db
Sameer Gupta <gamingwithsam0@gmail.com>
2026-05-26 00:01:50 +0100

    [sg2525] abstracted useful functions to global helper functions

--- src/emulator/processor.c
+++ src/emulator/processor.c
@@ -5,16 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "execute.h"
-
-
-// Select parts of the instruction register
-static uint32_t mask_instr_bits(uint32_t instruction, uint8_t start, uint8_t end){
-    uint8_t diff = start-end+1;
-    uint32_t shifted = (uint32_t)(instruction >> end);
-    uint32_t mask = (((uint32_t)1<<(diff))-1);
-    uint32_t res = shifted & mask;
-    return res;
-}
+#include "utils.h"
 
 // Load 32bit word from main memory.
 static uint32_t load32(Sys_Memory *mem, uint64_t address) {
