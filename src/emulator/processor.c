8300ffcc08ea10dba502f8b67b8ad1899305c12b
Vivek, Anish <av1525@ic.ac.uk>
2026-05-26 07:59:11 +0100

    [av1525] moved load32 to utils.h

--- src/emulator/processor.c
+++ src/emulator/processor.c
@@ -7,24 +7,8 @@
 #include "execute.h"
 #include "utils.h"
 
-// Load 32bit word from main memory.
-static uint32_t load32(Sys_Memory *mem, uint64_t address) {
-  // Checking for possible access errors.
-  if (address + 4 > MEMORY_SIZE) {
-    fprintf(stderr, "Address 0x%08lx is out of bounds\n", address);
-    exit(EXIT_FAILURE);
-  }
-  // We access data according to little-endian ordering.
-  uint32_t res = (uint32_t)mem->data[address] |
-                 (uint32_t)mem->data[address + 1] << 8 |
-                 (uint32_t)mem->data[address + 2] << 16 |
-                 (uint32_t)mem->data[address + 3] << 24;
-
-  return res;
-}
 
 // Initialising CPU State
-
 CPU_state state_init(Sys_Memory* memory){
     PSTATE pstate = {false, false, false, false};
     CPU_state res = {{0}, 0, pstate, memory};
