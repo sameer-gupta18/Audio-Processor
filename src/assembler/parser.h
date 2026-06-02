b7c3e5714bec7d91c6c418bb7bdfae743a1e4743
Vivek, Anish <anish.vivek25@imperial.ac.uk>
2026-06-02 10:51:39 +0100

    [av1525] completed symbol table

--- src/assembler/parser.h
+++ src/assembler/parser.h
@@ -12,5 +12,4 @@ int parser(
 );
 
 
-
 #endif
--- src/assembler/symtable.h
+++ src/assembler/symtable.h
@@ -1,13 +1,27 @@
 #ifndef SYMTABLE_H
 #define SYMTABLE_H
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define INIT_CAPACITY 32 
+#define GROWTH_RATE 2 
+#define LOAD_FACTOR 0.75
+typedef struct Node {
+    char* label;
+    uint32_t address; 
+    struct Node *next;
+} Node; 
 
 typedef struct{
-    uint8_t temp;
+    Node **buckets;
+    int capacity; 
+    int size; 
 } Sym_Table; 
 
-int insert_address(Sym_Table* sym_table, char* label, uint32_t address);
-uint32_t search_label(Sym_Table* sym_table, char* label);
-
+bool insert_address(Sym_Table* sym_table, char* label, uint32_t address);
+bool search_label(Sym_Table* sym_table, char* label, uint32_t*out);
+Sym_Table* symtable_init(void);
+void symtable_free(Sym_Table* st);
 
 #endif
