352866766659c97eab81a6369fa67a4402b83873
Sameer Gupta <sg2525@curve02.doc.ic.ac.uk>
2026-06-10 15:35:00 +0100

    fix: minor fixes in led_blink.s

--- src/raspberry_pi/led_blink.s
+++ src/raspberry_pi/led_blink.s
@@ -1,17 +1,17 @@
-movz w0 #0x40
-movz w1 #0x3f200000
-str w0 [w1]
-movz w2 #0x1c
-movz w3 #0x4
-movz w5 #0x0028
+movz x0, #0x40
+movz x1, #0x3f2, lsl #16
+str w0, [x1]
+movz x2, #0x1c
+movz x3, #0x4
+movz x5, #0x0028
 loop:
-    str w3 [w1,w2]
-    movz w4 #0x00FFF
+    str w3, [x1,x2]
+    movz x4, #0xFFFFFF
     delay: 
         subs w4, w4, #1
         b.ne delay
-    str w3 [w1,w5]
-    movz w4 #0x00FFF
+    str w3, [x1,x5]
+    movz x4, #0xFFFFFF
     delay2: 
         subs w4, w4, #1
         b.ne delay2
