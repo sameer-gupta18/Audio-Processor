4bef2ccab1f94fd9176f3b71f49d78a0faf708e0
Sameer Gupta <sg2525@ash32.doc.ic.ac.uk>
2026-06-16 17:55:17 +0100

    [av1525, pb624] fix: fixed initialization of reverb state

--- src/extension/effects/reverb.c
+++ src/extension/effects/reverb.c
@@ -1,9 +1,15 @@
 #include "effect.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
-#define INPUT_GAIN 0.015f;
-#define ALLPASS_COEFFICIENT 0.5f;
+#define INPUT_GAIN 0.015f
+#define ALLPASS_COEFFICIENT 0.5f
+#define NUM_COMBS 8
+#define NUM_ALLPASS 4
+
+static const int comb_lengths[8] = {1116,1188,1277,1356,1422,1491,1557, 1617};
+static const int all_pass_lengths[4] = {556,441,341,225};
 
 //comb structure
 typedef struct {
@@ -20,8 +26,8 @@ typedef struct {
 
 //reverb state
 typedef struct { 
-    comb c[8];
-    allpass a[4];
+    comb c[NUM_COMBS];
+    allpass a[NUM_ALLPASS];
     int rate;
 } reverb_state ; 
 
@@ -44,13 +50,20 @@ static inline float allpass_tick(allpass *a, float in) {
 //reset sets all the buffers to 0
 static void reverb_reset(fx *self) {
     reverb_state *s = self->state;
-    for(int i = 0; i < 8; i++) {
-        s->c->buf[i] = 0.0f;
+    for(int i = 0; i < NUM_COMBS; i++) {
+        s->c[i].store = 0.0f;
+        s->c[i].ind = 0;
+        for (int j = 0; j < s->c[i].len; j++){
+                  s->c[i].buf[j] = 0.0f;
+        }
     }
-    for(int i = 0; i < 4; i++) {
-        s->a->buf[i] = 0.0f;
+    for(int i = 0; i < NUM_ALLPASS; i++) {
+        s->a[i].ind = 0;
+        for (int j = 0; j < s->a[i].len; j++){
+            s->a[i].buf[j] = 0.0f;
+        }
     }
-    s->c->store = 0.0f;
+    
 }
 
 static void reverb_process(fx *self, float *buf, int n, float amt) {
@@ -67,7 +80,7 @@ static void reverb_process(fx *self, float *buf, int n, float amt) {
         }
         for (int j = 0; j < 4; j++) {
             //allpass runs in series
-            wet = allpass_tick(&s->a[j], in);
+            wet = allpass_tick(&s->a[j], wet);
         }
         buf[i] = (1.0f - mix) * buf[i] + mix * wet;
     }
@@ -84,7 +97,24 @@ fx *fx_reverb_create(int sample_rate){
     }
 
     s -> rate = sample_rate;
-    
+    for (int i = 0; i < NUM_COMBS; i++){
+        s->c[i].len = comb_lengths[i];
+        s->c[i].buf = calloc(s->c[i].len,sizeof(float));
+        s->c[i].ind = 0;
+        s->c[i].store = 0.0f;
+        if (s->c[i].buf == NULL){
+            fprintf(stderr, "Could not allocate memory to comb buffer");
+        }
+    }
+
+     for (int i = 0; i < NUM_ALLPASS; i++){
+        s->a[i].len = all_pass_lengths[i];
+        s->a[i].buf = calloc(s->a[i].len,sizeof(float));
+        s->a[i].ind = 0;
+        if (s->a[i].buf == NULL){
+            fprintf(stderr, "Could not allocate memory to all pass buffer");
+        }
+    }
 
     f -> name = "REVERB";
     f -> reset = reverb_reset;
