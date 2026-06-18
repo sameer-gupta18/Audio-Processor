6dea97dcf0a877ce6ffc80c1fa32f5ae2e878543
Sameer Gupta <sg2525@vertex13.doc.ic.ac.uk>
2026-06-18 17:43:48 +0100

    [sg2525] added comments to distortion effect

--- src/extension/effects/distortion.c
+++ src/extension/effects/distortion.c
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define DRIVE_C 1.0f
+#define DRIVE_M 19.0f
 
 typedef struct { int rate; } distortion_state;
 
@@ -11,16 +13,22 @@ static void distortion_process(fx *self, float *buf, int n, float amt){
     
     (void)self;
 
-    float drive = 1.0f + 19.0 * amt;
+    //setting a "crunch" level using the drive
+    float drive = DRIVE_C + DRIVE_M * amt;
 
+    //define the frequency modulation using a normalizing function over the drive
     float norm = tanhf(drive);
     for (int i = 0; i < n; i++){
         float x = buf[i];
+        
+        //ensures clean and controlled distortion
+
         float wet = tanhf(drive * x) / norm;
         buf[i] = (1.0f - amt) * x + amt * wet;
     } 
 }
 
+//generate distortion effect struct
 fx *fx_distortion_create(int sample_rate){
     fx *f = calloc(1, sizeof *f);
     distortion_state *s = calloc(1, sizeof *s);
